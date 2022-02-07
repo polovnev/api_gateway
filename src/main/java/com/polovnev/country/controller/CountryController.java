5fa095ca9f0c585e4378fd0b79522897ad321290
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-07 10:56:52 +0200

    remove redundant imports

--- src/main/java/com/polovnev/country/controller/CountryController.java
+++ src/main/java/com/polovnev/country/controller/CountryController.java
@@ -4,13 +4,11 @@ package com.polovnev.country.controller;
 import com.polovnev.country.dto.CountryDto;
 import com.polovnev.country.facade.CountryFacade;
 import com.polovnev.country.service.RabbitMessageSenderService;
-import com.polovnev.country.service.RestMessageSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/country")
