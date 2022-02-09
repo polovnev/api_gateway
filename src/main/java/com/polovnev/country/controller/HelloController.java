2415775e4fe43a9c3390cd23de6018b8a67ea286
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-09 23:26:15 +0200

    implement Spring Cloud Contract test

---
+++ src/main/java/com/polovnev/country/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.polovnev.country.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/hello")
+    public String getHello() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "http://localhost:8000/country/hello",
+                HttpMethod.GET,
+                new HttpEntity<>(httpHeaders),
+                String.class);
+
+        return responseEntity.getBody();
+    }
+}
---
+++ src/test/java/com/polovnev/country/integrational/BasicHelloControllerTest.java
@@ -0,0 +1,41 @@
+package com.polovnev.country.integrational;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = "com.polovnev:country:+:stubs:8000")
+public class BasicHelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void given_WhenPassHello_ThenReturnHello()
+            throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+}
