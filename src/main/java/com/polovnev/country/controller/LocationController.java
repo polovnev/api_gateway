1930d3727869325e3b9f9130c89d9472186607cb
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-16 17:31:11 +0200

    fix endpoint findByCountryId

--- src/main/java/com/polovnev/country/controller/LocationController.java
+++ src/main/java/com/polovnev/country/controller/LocationController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URISyntaxException;
 
-
+@RestController
+@RequestMapping("/country/{countryId}/location")
 public class LocationController {
 
     @Autowired
