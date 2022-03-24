db9142868f1cf92eede65a9e7b2be489e2d330c0
oleksandr_polovnev <polovnevad@gmail.com>
2022-03-24 23:42:02 +0100

    findByLocationId

--- src/main/java/com/polovnev/api_gateway/controller/LocationController.java
+++ src/main/java/com/polovnev/api_gateway/controller/LocationController.java
@@ -1,5 +1,6 @@
 package com.polovnev.api_gateway.controller;
 
+import com.polovnev.api_gateway.dto.LocationDto;
 import com.polovnev.api_gateway.facade.LocationFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +17,10 @@ public class LocationController {
     @GetMapping
     public String findByCountryId(@PathVariable Long countryId) throws URISyntaxException {
         return locationFacade.findByCountryId(countryId);
+    }
+
+    @GetMapping("/{locationId}")
+    public LocationDto findByLocationId(@PathVariable Long locationId) throws URISyntaxException {
+        return locationFacade.getLocationById(locationId);
     }
 }
--- src/main/java/com/polovnev/api_gateway/facade/LocationFacade.java
+++ src/main/java/com/polovnev/api_gateway/facade/LocationFacade.java
@@ -1,9 +1,9 @@
 package com.polovnev.api_gateway.facade;
 
+import com.polovnev.api_gateway.dto.LocationDto;
 import com.polovnev.api_gateway.service.RestMessageSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.net.URISyntaxException;
@@ -18,9 +18,13 @@ public class LocationFacade {
     @Value("${service.url.location}")
     private String baseUrlLocation;
 
-
     public String findByCountryId(Long countryId) throws URISyntaxException {
         String uri = baseUrlLocation + "/country/" + countryId + "/location";
         return restMessageSenderService.sendGetRequest(uri, String.class);
+    }
+
+    public LocationDto getLocationById(Long locationId) throws URISyntaxException {
+        String uri = baseUrlLocation + "/country/null/location/" + locationId;
+        return restMessageSenderService.sendGetRequest(uri, LocationDto.class);
     }
 }
